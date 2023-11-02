43430b5716b6ad17d1638a29cd07ebe8210aa969
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-11-02 22:36:32 +0300

    Update MainActivity2.java

--- app/src/main/java/com/selimcinar/multipleactivities/MainActivity2.java
+++ app/src/main/java/com/selimcinar/multipleactivities/MainActivity2.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
+ 
 public class MainActivity2 extends AppCompatActivity {
 
     TextView textView;
